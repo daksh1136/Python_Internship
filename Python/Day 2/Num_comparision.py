8db105ef4edc73f06bf81fa2d95dfa7477dcca2d
Daksh Sharma <dakshsharma1136@gmail.com>
2026-07-19 00:11:50 +0530

    Day 2

---
+++ Python/Day 2/Num_comparision.py
@@ -0,0 +1,7 @@
+n1=int(input("enter the first number"))
+n2=int(input("enterr second numbe"))
+if n1>n2:
+    print(f"{n1}is greater than {n2}")
+else:
+        print(f"{n2}is greater than {n1}")
+
---
+++ Python/Day 2/Vote_eligiblity.py
@@ -0,0 +1,5 @@
+age=int(input("enter your age to vote"))
+if age >=18 :
+    print(" you're eligible to vote")
+else:
+    print("youre not eligible for vote")
