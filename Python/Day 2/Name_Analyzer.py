53b118c791deae555b24bde816bc9d993970cff2
Daksh Sharma <dakshsharma1136@gmail.com>
2026-07-18 23:11:39 +0530

    Day2

---
+++ Python/Day 2/Name_Analyzer.py
@@ -0,0 +1,10 @@
+name=input("enter your name")
+print("------------NameAnalysis---------")
+
+
+print(name)
+print(name.capitalize)
+print(name.upper())
+print(name.lower())
+print(name[0:-1])
+print(name[1:7])
