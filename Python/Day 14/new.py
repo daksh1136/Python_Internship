41e33a18d3c090693b98cfbf00ab9449ccd55fb7
Daksh Sharma <dakshsharma1136@gmail.com>
2026-08-11 02:51:30 +0530

    Day 14

--- Python/Day 14/new.py
+++ Python/Day 14/new.py
@@ -1,11 +1,1 @@
-import pandas as pd
-
-data = {
-    "Name": ["Daksh", "Rahul", "Aman"],
-    "Age": [21, 20, 22],
-    "Course": ["CSE", "AIML", "CSE"]
-}
-
-df = pd.DataFrame(data)
-
-print(df)
+pip freeze > requirements.txt   
