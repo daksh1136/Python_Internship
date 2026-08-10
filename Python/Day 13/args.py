de611bbc97854fbf528cc4ac1502b0ef7465bb5e
Daksh Sharma <dakshsharma1136@gmail.com>
2026-08-11 02:27:40 +0530

    Day 13

---
+++ Python/Day 13/args.py
@@ -0,0 +1,9 @@
+def ar(nums,*num):
+    print(nums)
+    for items in num:
+        print(num)
+
+km=[1,2,3,4,5,6,7,8,55
+]
+km.append(5)
+ar("ki",km)
---
+++ Python/Day 13/kwargs.py
@@ -0,0 +1,8 @@
+def kwar(**kw):
+    for key,value in kw.items():
+        print(f"{key} : {value}")
+
+
+kwar(name="daksh",age=20,city="delhi")  
+
+k
